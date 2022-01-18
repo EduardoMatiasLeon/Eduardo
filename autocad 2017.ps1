b596bb7388ecc3cff887fbe547ee962329423956
EduardoMatiasLeon <97908617+EduardoMatiasLeon@users.noreply.github.com>
2022-01-18 12:51:02 -0600

    Add files via upload

---
+++ autocad 2017.ps1
@@ -0,0 +1,4 @@
+﻿New-Item -Path "c:\" -Name "CAD" -ItemType "directory"
+powershell -Command {\\10.76.16.26\Programas\AutoCAD2017\AutoCAD_2017_English_Win_64bit_dlm_001_002.sfx.exe -suppresslaunch -d "C:\CAD"}
+Start-Sleep -s 240
+powershell -Command {\\10.76.16.26\CAD\AutoCAD_2017_English_Win_64bit_dlm\Setup.exe /W /Q /I .\setup.ini}
