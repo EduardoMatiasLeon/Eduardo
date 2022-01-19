0763529340b7532cfa58978f32d6d9be147220ed
EduardoMatiasLeon <97908617+EduardoMatiasLeon@users.noreply.github.com>
2022-01-18 19:27:38 -0600

    Update autocad 2017.ps1

--- autocad 2017.ps1
+++ autocad 2017.ps1
@@ -1,3 +1,4 @@
 ﻿New-Item -Path "c:\" -Name "CAD" -ItemType "directory"
 powershell -Command {\\10.76.16.26\Programas\AutoCAD2017\AutoCAD_2017_English_Win_64bit_dlm_001_002.sfx.exe -suppresslaunch -d "C:\CAD"}
-powershell -Command {C:\CAD\AutoCAD_2017_English_Win_64bit_dlm\Setup.exe /W /Q /I .\setup.ini}
+Start-Sleep -s 540
+powershell -Command {C:\CAD\AutoCAD_2017_English_Win_64bit_dlm\Setup.exe .\setup.ini}
