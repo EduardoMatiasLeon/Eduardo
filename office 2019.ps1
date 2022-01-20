405b29d539b9d8675c756ac298693c33d345f093
EduardoMatiasLeon <97908617+EduardoMatiasLeon@users.noreply.github.com>
2022-01-20 11:15:26 -0600

    Update office 2019.ps1

--- office 2019.ps1
+++ office 2019.ps1
@@ -1,1 +1,1 @@
-powershell -Command {\\10.76.16.26\c\Users\Administrator\Desktop\Programas\5Office2019\setup.exe /I /Q}
+powershell -Command {\\10.76.16.26\c\Users\Administrator\Desktop\Programas\5Office2019\setup.exe /I}
