74b506502ad69c136582ce64c9f4720e175b5c12
EduardoMatiasLeon <97908617+EduardoMatiasLeon@users.noreply.github.com>
2022-01-20 19:35:54 -0600

    Update office 2019.ps1

--- office 2019.ps1
+++ office 2019.ps1
@@ -1,1 +1,1 @@
-powershell -Command {\\10.76.16.26\c\Users\Administrator\Desktop\Programas\5Office2019\setup.exe /I .\configoffice.xml}
+powershell -Command {\\10.76.16.26\c\Users\Administrator\Desktop\Programas\5Office2019\setup.exe /download descargar office.xml}
